bba0df9d5bdec637ba80dd3e5c07319d6854989a
manrajchauhan <m900413089@gmail.com>
2024-02-04 21:05:04 +0530

    Update at contact api

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,7 +5,6 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, message, name } = req.body;
-
   // validate the data coming in
   if (!email || !message || !name) {
     return res.status(400).send("Missing one or more fields");
@@ -13,7 +12,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const msg = {
     to: process.env.EMAIL_TO,
-    from: email,
+    from: {
+      email: process.env.EMAIL_FROM,
+      name: `${name} via Portfolio`,
+    },
     replyTo: email,
     subject: `Message from ${name}`,
     name,
@@ -24,7 +26,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     await sgMail.send(msg);
     res.json({ message: `Email has been sent` });
   } catch (error) {
-    console.error("Error sending email:", error);
     res.status(500).json({ error: "Error sending email" });
   }
 };
